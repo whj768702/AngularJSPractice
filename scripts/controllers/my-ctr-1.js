18d61206e75f48b391903562b63aab0a423540ac
whj768702 <whj768702@gmail.com>
2016-10-17 21:33:22 +0800

    删除不必要文件。在bower中添加ui.router依赖。

--- scripts/controllers/my-ctr-1.js
+++ scripts/controllers/my-ctr-1.js
@@ -4,6 +4,6 @@ define(['./controllers'], function(controllers){
 
     controllers.controller('MyCtrl1', ['$scope', function($scope){
         $scope.hello = 'hello';
-        console.log($scope.hello);
+        $scope.message = 'hello';
     }]);
 });
